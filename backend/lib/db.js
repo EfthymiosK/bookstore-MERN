9be5bc44d58c88b9371025a4a0965f5593f0e6ce
EfthymiosK <113445298+EfthymiosK@users.noreply.github.com>
2025-03-20 14:16:00 +0200

    Update db.js

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
-const MONGODB_URL =
-  "mongodb+srv://mkokmotos:rryUs2LCkyaqZO5Z@bookstore-db.f5lk3.mongodb.net/?retryWrites=true&w=majority&appName=bookstore-db";
+
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URL);
